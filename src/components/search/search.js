ebfca54192e183cac5ee71471b753f2cf0e0a14d
martynas3113 <martynas.pazusis@gmail.com>
2021-02-04 00:06:11 +0200

    validation added

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -4,20 +4,42 @@ import  './search.scss';
 
 const Search = ({onSearch}) => {
   const [title, setTitle] = useState("");
+  const [isError, setIsError] = useState(false);
 
  const onSearchChanged = (e) => {
     const title = e.target.value;
     setTitle(title);
-
+    
   };
 
-  const onSubmit = (e) => {
+  const showResults = (e) => {
     e.preventDefault();
-    onSearch(title);
+    if(!formValidation()){
+      setIsError(false);
+       onSubmit();
+    }
+    else{
+      setIsError(true);
+      console.log("did not pass")
+    }
+  }
+  
+  const onSubmit = () => {
+      onSearch(title);
   };
+  const formValidation = () => {
+    const nameValid = /[^A-Za-z]/;
+    if (nameValid.test(title)) {
+        return true;
+    } else {
+      return false;
+    }
+
+
+}
   return (
     <>
-          <form onSubmit={onSubmit} className="search-form">
+          <form onSubmit={showResults} className="search-form">
             <div className="form-controls">
               <label>Search</label>
               <input
@@ -28,8 +50,9 @@ const Search = ({onSearch}) => {
                 placeholder="Enter Search Keyword"
               />
             </div>
-            <button onClick={onSubmit} className="btn btn-primary">Go</button>
+            <button onClick={showResults} className="btn btn-primary">Go</button>
           </form>
+          <div className={isError ? "error active" : "error"}>Error</div>
         </>
   )
 }
--- src/components/video/video.js
+++ src/components/video/video.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import './video.scss';
 
-function selectVideo(videoIdObj, onVideoSelected) {
-  window.scrollTo({
-    top:0
-  })
-  onVideoSelected(videoIdObj.videoId);
-}
-
-function constructVideoTitles(vidoesData, onVideoSelected) {
-  return vidoesData.map(({ snippet, id }, index) => {
-    return (
-      <div
-        className="video"
-        key={index}
-        onClick={() => selectVideo(id, onVideoSelected)}>
-        <div className="image-bg" style={{backgroundImage: `url(${snippet.thumbnails.high.url})`}} key={index} />
-        <p className="title">{snippet.title}</p>
-      </div>
-    );
-  });
-}
 const Video = ({ data, onVideoSelected }) => {
+
+  const selectVideo = (videoIdObj, onVideoSelected) => {
+    window.scrollTo({
+      top:0
+    })
+    onVideoSelected(videoIdObj.videoId);
+  }
+
+  const constructVideoTitles = (videosData, onVideoSelected) => {
+    return videosData.map(({ snippet, id }, index) => {
+      return (
+        <div
+          className="video"
+          key={index}
+          onClick={() => selectVideo(id, onVideoSelected)}>
+          <div className="image-bg" style={{backgroundImage: `url(${snippet.thumbnails.high.url})`}} key={index} />
+          <p className="title">{snippet.title}</p>
+        </div>
+      );
+    });
+  }
   return <>{constructVideoTitles(data, onVideoSelected)}</>;
 };
 
