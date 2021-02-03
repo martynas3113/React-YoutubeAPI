366e61105e9eecff053f3b21cca68289ffbca196
martynas3113 <martynas.pazusis@gmail.com>
2021-02-03 22:10:35 +0200

    refactory

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,35 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import  './search.scss';
 
-class Search extends React.Component {
-    state = { title: "" };
-    onSearchChanged = event => {
-      const _title = event.target.value;
-      this.setState({ title: _title });
-    };
-    onSubmit = event => {
-      event.preventDefault();
-      this.props.onSearch(this.state.title);
-    };
-    render() {
-      return (
-        <>
-          <form onSubmit={this.onSubmit} className="search-form">
+
+const Search = ({onSearch}) => {
+  const [title, setTitle] = useState("");
+
+ const onSearchChanged = (e) => {
+    const title = e.target.value;
+    setTitle(title);
+
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onSearch(title);
+  };
+  return (
+    <>
+          <form onSubmit={onSubmit} className="search-form">
             <div className="form-controls">
               <label>Search</label>
               <input
                 id="video-search"
                 type="text"
-                value={this.state.title}
-                onChange={this.onSearchChanged}
+                value={title}
+                onChange={onSearchChanged}
                 placeholder="Enter Search Keyword"
               />
             </div>
-            <button onClick={this.onSubmit} className="btn btn-primary">Go</button>
+            <button onClick={onSubmit} className="btn btn-primary">Go</button>
           </form>
         </>
-      );
-    }
-  }
-  
-  export default Search;
+  )
+}
+
+export default Search
+
