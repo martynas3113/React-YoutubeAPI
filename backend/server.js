0624288c102d1fe1d3346303b0d757a685ad63cf
martynas3113 <martynas.pazusis@gmail.com>
2021-02-05 15:50:42 +0200

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const path = require("path");
+app.use(express.static(path.join(__dirname, "client/build")));
 
 const mongoose = require('mongoose');
 
